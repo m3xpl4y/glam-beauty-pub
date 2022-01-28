423470792c36999035087e04a5da80fb40242c2c
m3xpl4y <m3xpl4y@gmail.com>
2022-01-28 19:29:10 +0100

    fetch array

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,19 @@
+import { useState } from "react";
+
 const Home = () => {
+    const [blogs, setBlogs] = useState([
+        {title: 'My new website', body: 'This is my new website', author: 'Max', id: 1},
+        {title: 'Welcome to party!', body: 'This is the party blog', author: 'Angi', id: 2},
+        {title: 'Web dev top tips', body: 'From dev to beginner', author: 'Max', id: 3}
+    ]);
     return ( 
         <div className="home">
-            <h4>Home Page</h4>
+            {blogs.map((blog)=>(
+                <div className="blog-preview" key={blog.id}>
+                    <h4>{blog.title}</h4>
+                    <p>Written by {blog.author}</p>
+                </div>
+            ))}
         </div>
      );
 }
