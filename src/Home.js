ee8c51469b79605ef7f33c8b045ef535dc2cbf4f
m3xpl4y <m3xpl4y@gmail.com>
2022-01-28 18:19:02 +0100

    new style and components

---
+++ src/Home.js
@@ -0,0 +1,9 @@
+const Home = () => {
+    return ( 
+        <div className="home">
+            <h4>Home Page</h4>
+        </div>
+     );
+}
+ 
+export default Home;
---
+++ src/NavBar.js
@@ -0,0 +1,13 @@
+const NavBar = () => {
+    return ( 
+        <nav className="navbar">
+            <h3>Glambeauty Pub</h3>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">New Blog</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default NavBar;
